c57e045c0081769e4d6c9c056fefc472b7570a2b
YasmeenMoElwakil <Yasmeen.M.Elwakil@gmail.com>
2025-06-21 17:17:45 +0300

    CT Driver License

---
+++ CT_DL.py
@@ -0,0 +1,16 @@
+currant_year = input("what is the currant year?: ")
+currant_year = int(currant_year) 
+name = input("Enter your name: ") 
+print("Hello, " + name + "!")
+
+year_of_birth = input("Enter your year of birth: ")
+year_of_birth = int(year_of_birth)
+age = currant_year - year_of_birth
+age = str(age) 
+print("So your name is " + name + " and you are " + age + " years old, correct?") 
+age = int(age)
+if age>=16:
+    print ("You can apply for a CT DL")
+
+else:
+    print ("You can NOT apply for a CT DL")
